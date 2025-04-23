ba75d9d092bf90a783b1410b3d35d0da4cb4ab46
G476-droid <71474112+G476-droid@users.noreply.github.com>
2025-04-22 21:42:25 -0500

    Update eliminar.php

--- eliminar.php
+++ eliminar.php
@@ -1,6 +1,22 @@
 <?php
 include "db.php";
-$id = $_GET['id'];
-$persona_id = $_GET['persona_id'];
-$conn->query("DELETE FROM actividades WHERE id = $id");
-header("Location: actividades.php?persona_id=$persona_id");
+
+// Validar y castear parámetros
+$id = isset($_GET['id']) ? (int) $_GET['id'] : 0;
+$persona_id = isset($_GET['persona_id']) ? (int) $_GET['persona_id'] : 0;
+
+// Ejecutar el DELETE usando pg_query_params
+$result = pg_query_params(
+    $conn,
+    "DELETE FROM actividades WHERE id = $1",
+    array($id)
+);
+
+if (!$result) {
+    die("Error al eliminar la actividad: " . pg_last_error($conn));
+}
+
+// Redirigir de vuelta a la lista de actividades
+header("Location: actividades.php?persona_id=" . $persona_id);
+exit;
+?>
