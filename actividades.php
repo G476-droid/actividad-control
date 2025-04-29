5d2ccd7d296fc8decd8b1d22fd48694bb72a706c
G476-droid <71474112+G476-droid@users.noreply.github.com>
2025-04-28 19:49:25 -0500

    Update actividades.php8

--- actividades.php
+++ actividades.php
@@ -55,6 +55,7 @@ if ($es_admin) {
 <body class="container py-4">
   <h2>Actividades de <?= htmlspecialchars($persona['nombre']) ?></h2>
   <a href="nueva_actividad.php?persona_id=<?= $persona_id ?>" class="btn btn-sm btn-success mb-3">+ Nueva Actividad</a>
+  <a href="nueva_actividad.php?persona_id=<?= $persona_id ?>" class="btn btn-sm btn-success mb-3">Atividades Completadas</a>
   <a href="logout.php" class="btn btn-sm btn-outline-danger float-end">Cerrar sesión</a>
   <table class="table table-bordered">
    <thead>
