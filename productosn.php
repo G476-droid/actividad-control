1b1f5780efd422a60e3f872035ded3a4e15f08ff
G476-droid <71474112+G476-droid@users.noreply.github.com>
2025-05-02 23:01:25 -0500

    Update productosn.php

--- productosn.php
+++ productosn.php
@@ -38,7 +38,7 @@ if ($termino !== '') {
 <body>
 
 <h2 class="mb-4">Productos Novopan</h2>
-    <a href="admin.php" class="btn btn-outline-dark mb-4">← Volver al Menú Principal</a>
+    <a href="vista_admin.php" class="btn btn-outline-dark mb-4">← Volver al Menú Principal</a>
 
 <form method="GET" class="mb-4">
   <div class="input-group w-50">
