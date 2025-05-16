d984500ef89221b91986dd22beb5e576eba6dcbb
G476-droid <71474112+G476-droid@users.noreply.github.com>
2025-05-15 21:46:14 -0500

    Update db.php

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
-$host = "dpg-d03gkjadbo4c738camp0-a.oregon-postgres.render.com";
+$host = "dpg-d0jacaeuk2gs73bltelg-a.oregon-postgres.render.com";
 $user = "actividadesmyv20251_db";
-$password = "7VovdXaqf7hoID2n6CkBrxgLx9rEmaoJ";
-$dbname = "actividadesmyv20251_db_6phh";
+$password = "Yxs6lcIE8T6Fe8sj6v0IuNCdzde9Pdda";
+$dbname = "actividadesmyv20251_db_mpfc";
 $port = "5432";
 
 $conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
